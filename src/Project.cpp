DavidBailes/audacity

commit cfeb7bced169f960e71275a90bfd6f3ad88e501c

Zoom to fit after any import (labels, audio, or midi)

src/Project.cpp
@@ -463,6 +463,8 @@ public:
          for (const auto &name : sortednames)
             mProject->Import(name);
 
+         mProject->ZoomAfterImport(nullptr);
+
          return true;
       } );
    }
@@ -4171,12 +4173,6 @@ void AudacityProject::AddImportedTracks(const wxString &fileName,
    wxEventLoopBase::GetActive()->YieldFor(wxEVT_CATEGORY_UI | wxEVT_CATEGORY_USER_INPUT);
 #endif
 
-   OnZoomFit();
-
-   mTrackPanel->SetFocus();
-   mTrackPanel->EnsureVisible(mTrackPanel->GetFirstSelectedTrack());
-   mTrackPanel->Refresh(false);
-
    if (initiallyEmpty && mDirManager->GetProjectName() == wxT("")) {
       wxString name = fileName.AfterLast(wxFILE_SEP_PATH).BeforeLast(wxT('.'));
       mFileName =::wxPathOnly(fileName) + wxFILE_SEP_PATH + name + wxT(".aup");
@@ -4188,6 +4184,17 @@ void AudacityProject::AddImportedTracks(const wxString &fileName,
    //   HandleResize();
 
    newTracks.clear();
+}
+
+void AudacityProject::ZoomAfterImport(Track *pTrack)
+{
+   OnZoomFit();
+
+   mTrackPanel->SetFocus();
+   RedrawProject();
+   if (!pTrack)
+      pTrack = mTrackPanel->GetFirstSelectedTrack();
+   mTrackPanel->EnsureVisible(pTrack);
 }
 
 // If pNewTrackList is passed in non-NULL, it gets filled with the pointers to NEW tracks.

src/Menus.cpp
@@ -6733,6 +6733,8 @@ void AudacityProject::OnImport()
 
       Import(fileName);
    }
+
+   ZoomAfterImport(nullptr);
 }
 
 void AudacityProject::OnImportLabels()
@@ -6776,7 +6778,7 @@ void AudacityProject::OnImportLabels()
                 Format(_("Imported labels from '%s'"), fileName.c_str()),
                 _("Import Labels"));
 
-      RedrawProject();
+      ZoomAfterImport(nullptr);
    }
 }
 
@@ -6821,8 +6823,7 @@ AudacityProject *AudacityProject::DoImportMIDI(
       pProject->PushState(wxString::Format(_("Imported MIDI from '%s'"),
          fileName.c_str()), _("Import MIDI"));
 
-      pProject->RedrawProject();
-      pProject->mTrackPanel->EnsureVisible(pTrack);
+      pProject->ZoomAfterImport(pTrack);
       pNewProject = nullptr;
       return pProject;
    }

src/Project.h
@@ -265,6 +265,8 @@ public:
    // If pNewTrackList is passed in non-NULL, it gets filled with the pointers to NEW tracks.
    bool Import(const wxString &fileName, WaveTrackArray *pTrackArray = NULL);
 
+   void ZoomAfterImport(Track *pTrack);
+
    void AddImportedTracks(const wxString &fileName,
                           TrackHolders &&newTracks);
 

commit 0ed8fa27644c3c045c11a8962a57be61e13fdd40

1765: File>Open to import audio gives wrong zoom level and no scroll bar slider
Problem caused by commit cfeb7bc

Fix. The above commit removed a call to change the zoom level from AudacityProject::AddImportedTracks, and hence AudacityProject::Import, so add a call to ZoomAfterImport().

src/Project.cpp
@@ -3033,8 +3033,10 @@ void AudacityProject::OpenFile(const wxString &fileNameArg, bool addtohistory)
       // If it's not XML, try opening it as any other form of audio
       if (Importer::IsMidi(fileName))
          DoImportMIDI(this, fileName);
-      else
+      else {
          Import(fileName);
+         ZoomAfterImport(nullptr);
+      }
       return;
    }
 